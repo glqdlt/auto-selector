0ecf0e0af08f57fef90ea190578ea04c764a30c6
glqdlt <dlfdnd0725@gmail.com>
2020-07-02 17:49:47 +0900

    Init

---
+++ src/main/java/com/glqdlt/example/autoselectquery/AutoSelectQueryApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.example.autoselectquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSelectQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoSelectQueryApplication.class, args);
+    }
+
+}
